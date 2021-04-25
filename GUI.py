602739b1df75ccf28d194ca30f5d141baa043fc6
Ionut <ionutcosma067@gmail.com>
2021-04-25 13:22:56 +0100

    Scan functionality added + malicious code snippets

--- GUI.py
+++ GUI.py
@@ -6,11 +6,11 @@ At scale this application would not identify malicious code using strings, but b
 
 import PySimpleGUI as psg
 import os
-import shutil
 from malicious_codes_list import malicious_codes
+from threading import Thread
 
 # List of most commonly used for malicious code injection.
-target_filees = ('.py', '.exe', '.doc', '.docx', '.docm', '.xlsm', '.hta', '.html',
+target_files_ext = ('.py', '.exe', '.doc', '.docx', '.docm', '.xlsm', '.hta', '.html',
                 '.htm', '.js', '.jar', '.vbs', '.vb', 'pdf')
 
 #Create the window
@@ -20,8 +20,23 @@ layout = [[psg.Text('MalProtectAV', size=(10,2), key = "process_update")],
 window = psg.Window('MalProtect Antivirus', layout, margins=(150, 150))
 process_text = window['process_update']
 
-#def scan_target():
-#def scan_complete():
+# function call
+def scan_target():
+    with open('scan_results.txt', 'a+') as scan_results:
+        for root, dirs, files in os.walk('/'):
+            for filename in files:
+                if any(filename.endswith(extension) for extension in target_files_ext):
+                    targeted_file = os.path.join(root,filename)
+                    with open(targeted_file, errors = 'ignore') as read_file:
+                        signature_detection = read_file.read()
+                        for snippet in malicious_codes:
+                            if snippet in signature_detection:
+                                scan_results.write('[WARNING] ' + targeted_file + '[May be malicious] ' + '\n\n')
+
+if __name__ == "__main__":
+    thread = Thread(target = scan_target)
+    thread.start()
+    thread.join()
 
 # Run GUI in a loop
 while True:
