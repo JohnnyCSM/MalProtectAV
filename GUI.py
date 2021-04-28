af6a1685cb676e2595231628aec547448188ea74
Ionut <ionutcosma067@gmail.com>
2021-04-28 11:43:38 +0100

    GUI improved + threading + more features added

--- GUI.py
+++ GUI.py
@@ -3,55 +3,63 @@ A signature based detection tool that identifies malicious code.
 Helps in determining desired functionality of predefined executable files and code.
 '''
 
+import threading
 import PySimpleGUI as psg
 import os
 from malicious_codes_list import malicious_codes
 import shutil
-from fatrat_binaries import fatrat
+from btn_images import scan_btn, results_btn, quarantine_btn, exit_btn
 
 # List of most commonly used for malicious code injection.
 target_files_ext = ('.py', '.exe', '.doc', '.docx', '.docm', '.xlsm', '.hta', '.html',
                 '.htm', '.js', '.jar', '.vbs', '.vb', 'pdf')
 
-#Create the window
-layout = [[psg.Text('MalProtectAV', size=(10,2), key = "process_update")],
-          [psg.Text("Choose folder to scan: "), psg.FolderBrowse(key = "dir_to_scan")],
-          [psg.Button('Scan'), psg.Button('Results'), psg.Button('Quarantine'), psg.Button('Exit')]]
+thread_process = '-SCAN-'
 
-window = psg.Window('MalProtect Antivirus', layout, margins=(150, 150))
-process_text = window['process_update']
+fatrat_generated_files = [
+    'TheFatRat_Files/April_Invoice.exe',
+    'TheFatRat_Files/Bank_Statement.exe',
+    'TheFatRat_Files/Payroll20-04.exe',
+]
 
-def scan_complete():
-    process_text.update('Scan complete... Check results.')
 
-def q_complete():
-    process_text.update('Quarantine complete.')
-
-# function call
-def scan_target():
-    process_text.update('Scanning... please wait.')
+def scanner(window):
+    window.write_event_value('-SCAN-', ('Starting Scan.', values["dir_to_scan"]))
     scan_target_dir = values["dir_to_scan"]
     with open('scan_results.txt', 'a+') as scan_results, open('to_be_quarantined.txt', 'a+') as quarantine_results:
         for root, dirs, files in os.walk(scan_target_dir):
             for filename in files:
                 if any(filename.endswith(extension) for extension in target_files_ext):
                     targeted_file = os.path.join(root,filename)
+                    window.write_event_value('-SCAN-', ('Scanning... ', targeted_file))
                     with open(targeted_file, errors = 'ignore') as read_file:
                         signature_detection = read_file.read()
                         for snippet in malicious_codes:
                             if snippet in signature_detection:
-                                scan_results.write('[WARNING] ' + targeted_file + ' [May be malicious] ' + '\n\n')
+                                scan_results.write(
+                                    '[WARNING] ' + targeted_file + ' [MAY CONTAIN MALICIOUS CODE AND SHOULD BE QUARANTINED ] ' + '\n\n')
                                 quarantine_results.write(targeted_file + "\n")
-                        if filename.endswith('.exe'):
-                            with open(filename, 'r+b') as exe_binaries:
+                                window.write_event_value('-SCAN-', ('POSSIBLE VIRUS FOUND: ', targeted_file))
+                        if targeted_file.endswith('.exe'):
+                            with open(targeted_file, 'rb') as exe_binaries:
                                 signature_detection_hex = exe_binaries.read()
-                                for snippet in fatrat:
-                                    if snippet in signature_detection_hex:
-                                        scan_results.write(
-                                            '[WARNING] ' + targeted_file + '[May be a TheFatRat file and must be quarantined.]' + '\n\n')
-                                        quarantine_results.write(targeted_file + "\n")
-                                    
-    scan_complete()
+                                for files in fatrat_generated_files:
+                                    with open(files, 'rb') as check_file:
+                                        check_bins = check_file.read() 
+                                        if check_bins == signature_detection_hex:
+                                            scan_results.write(
+                                                '[WARNING] ' + targeted_file + '[MAY BE A TheFatRat GENERATED FILE AND SHOULD BE QUARANTINED.]' + '\n\n')
+                                            quarantine_results.write(targeted_file + "\n\n")
+                                            window.write_event_value('-SCAN-', (
+                                                'POSSIBLE FatRat FILE FOUND: ', targeted_file ))
+    window.write_event_value('-SCAN-', 'Scan Complete.' + '\n')
+
+
+def see_results():
+    with open('scan_results.txt',
+              'r') as view_results:
+        for lines in view_results:
+            print(lines)
 
 def quarantine_from_results():
     user_desktop = os.path.expanduser("~/Desktop")
@@ -62,21 +70,42 @@ def quarantine_from_results():
         for line in files_to_be_quarantined:
             try:
                 shutil.move(line.strip("\n"), q_directory)
-            except FileExistsError:
+            except:
                 continue
-    
-    q_complete()
+    print('Quarantined Files. Check quarantine!' + '\n')
 
-# Run GUI in a loop
-while True:
-    event, values = window.read()
-    if event == psg.WIN_CLOSED or event == 'Exit':
-        break
-    if event == "Scan":
-        scan_target()
-    elif event == 'Results':
-        process_text.update('Opening Results...')
-        os.system("notepad.exe scan_results.txt")
-    elif event == 'Quarantine':
-        process_text.update('Quarantine results...')
-        quarantine_from_results()
+
+def main():
+    global values
+    #Create the window
+    layout = [[psg.Text('MalProtectAV', font='Any 15', text_color = 'black', background_color = 'white')],
+              [psg.Text("Choose folder to scan: ", font='Any 15', text_color = 'black'), 
+               psg.FolderBrowse(key = "dir_to_scan")],
+
+              [psg.Multiline(size=(93, 20), key='-ML-', autoscroll=True, reroute_stdout=True, write_only=True,
+                                reroute_cprint=True)],
+              [psg.B('', image_data=scan_btn, button_color='white', key= 'Start'), 
+               psg.Button('', image_data=results_btn, button_color='white', key= 'Results'), 
+               psg.Button('', image_data=quarantine_btn, button_color='white', key= 'Quarantine'), 
+               psg.Button('', image_data=exit_btn, button_color='white', key= 'Exit')]]
+
+    window = psg.Window('MalProtect Antivirus', layout, background_color='white' )
+
+    # Run GUI in a loop
+    while True:
+        event, values = window.read()
+        if event == psg.WIN_CLOSED or event == 'Exit':
+            break
+        if event.startswith('Start'):
+            threading.Thread(target=scanner, args=(window,), daemon=False).start()
+        if event == thread_process:
+            psg.cprint(f'{values[thread_process]}', colors='white on red')
+        if event == 'Results':
+            see_results()
+        if event == 'Quarantine':
+            quarantine_from_results()
+    window.close()
+
+
+if __name__ == '__main__':
+    main()
